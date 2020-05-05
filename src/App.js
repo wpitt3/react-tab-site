dcdf441a19e9d707d4168e6671ec74e890f2c399
Will Pitt <will@carsnip.com>
2020-05-05 22:26:23 +0100

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Header, Grid, Segment, Button, List, TextArea } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTabText: "",
+      editModeInsert:false,
+      songs: []
+    };
+
+    this.handleEditModeChange = this.handleEditModeChange.bind(this);
+  }
+
+  buildEditButton(name, positive) {
+    if (positive) {
+      return ( <Button positive >{name}</Button>)
+    } else {
+      return ( <Button onClick={this.handleEditModeChange}>{name}</Button>)
+    }
+  }
+
+  handleEditModeChange(event) {
+    this.setState({editModeInsert: !this.state.editModeInsert});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Grid container columns={1} stackable>
+          <Header as='h1'>React Tab Editor </Header>
+          <Grid.Column>
+            <Segment className="action-buttons">
+            <List link className="tab-links">
+            </List>
+            <Button.Group>
+               <Button content='Save' onClick={this.handleSave}/>
+               <Button content='Load' onClick={this.handleLoad}/>
+              </Button.Group>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>
+              <Button.Group>
+                {this.buildEditButton('Insert', this.state.editModeInsert)}
+                <Button.Or />
+                {this.buildEditButton('Replace', !this.state.editModeInsert)}
+              </Button.Group>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment id="tab-text">
+              <TextArea autoHeight />
+            </Segment>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default App;
